c8cb9a255761aef7c7170330f383ba91fd9b7796
lozanoshop <154705099+lozanoshop@users.noreply.github.com>
2023-12-26 14:03:01 +0000

    Add files via upload

---
+++ dist/script.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function(){
+const loader = document.querySelector(".loader");
+setTimeout(function(){
+                loader.classList.add("hideLoader");
+}, 3000);
