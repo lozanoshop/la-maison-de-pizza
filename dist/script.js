54a01c6999247561c2fe3c43bce0719e2e20380c
lozanoshop <154705099+lozanoshop@users.noreply.github.com>
2023-12-27 10:18:23 +0000

    Update script.js

--- dist/script.js
+++ dist/script.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', function(){
 const loader = document.querySelector(".loader");
 setTimeout(function(){
-                var drive_link = "https://drive.google.com/file/d/1t--5h5r6kVNuPkxjhOcevuLRmcpeF21d/view?usp=sharing"
+                var drive_link = "https://drive.google.com/file/d/1X2BcmtVSu_41vNUb37k2tyxzPe4FkO1V/view?usp=sharing"
 window.location.href = drive_link;
 }, 1000)})
  
